3cf9ea972e04a9f0e4ddd65706cd167d49cc7fa1
Jonathan Clark <30505583+binkeroni@users.noreply.github.com>
2020-04-25 00:03:21 -0700

    fixed formatting on all files used

--- app.js
+++ app.js
@@ -7,24 +7,21 @@ const store = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'The shift() method removes the first element from an array and does what next?',
+      question:
+        'The shift() method removes the first element from an array and does what next?',
       answers: [
         'returns the next element in the array',
         'returns a shrug emoji in Unicode Hex',
         'returns the removed element',
-        'returns a copy of the array'
+        'returns a copy of the array',
       ],
-      correctAnswer: 'returns the removed element'
+      correctAnswer: 'returns the removed element',
     },
     {
-      question: 'The which method calls a function once for each element in an array?',
-      answers: [
-        'forTheWin()',
-        'forEach()',
-        'forCamelot()',
-        'forEvery()'
-      ],
-      correctAnswer: 'forEach()'
+      question:
+        'The which method calls a function once for each element in an array?',
+      answers: ['forTheWin()', 'forEach()', 'forCamelot()', 'forEvery()'],
+      correctAnswer: 'forEach()',
     },
     {
       question: 'The filter() method returns an array of what?',
@@ -32,19 +29,21 @@ const store = {
         'all elements that fail the test function',
         'index numbers of all elements that pass the test function',
         'all elements that pass the test function',
-        'all elements that are the specified data type'
+        'all elements that are the specified data type',
       ],
-      correctAnswer: 'all elements that pass the test function'
+      correctAnswer: 'all elements that pass the test function',
     },
     {
-      question: 'The map() method does what with a provided function as an argument?',
+      question:
+        'The map() method does what with a provided function as an argument?',
       answers: [
         'returns a map to El Dorado',
         'returns an object with the function name as the key and the original array as the value',
         'returns the orginal array with the results of the funcion called on each element',
-        'returns a new array with the results of the funcion called on each element in the orginal array'
+        'returns a new array with the results of the funcion called on each element in the orginal array',
       ],
-      correctAnswer: 'returns a new array with the results of the funcion called on each element in the orginal array'
+      correctAnswer:
+        'returns a new array with the results of the funcion called on each element in the orginal array',
     },
     {
       question: 'The every() method does what for each element in an array?',
@@ -52,14 +51,14 @@ const store = {
         'checks if elements pass a test as provided as a fucntion',
         'converts the elements to true or false depending the provided function',
         'creates a new array with each element being identical to the first element in the array',
-        'creates a new array with converting each element to \'every\''
+        "creates a new array with converting each element to 'every'",
       ],
-      correctAnswer: 'checks if elements pass a test as provided as a fucntion'
+      correctAnswer: 'checks if elements pass a test as provided as a fucntion',
     },
   ],
   quizStarted: false,
   questionNumber: 0,
-  score: 0
+  score: 0,
 };
 
 /**
@@ -83,7 +82,7 @@ const store = {
 /**
  * Generate HTML Start Screen
  */
-let startPageTemplate = function() {
+let startPageTemplate = function () {
   $('main').html(`<container>
             <section class="question-section">
 
@@ -95,12 +94,11 @@ let startPageTemplate = function() {
                 <button class="go-button" type="button">Go</button>
                 <button class="scared-button" type="button">No I'm scared</button>
             </section>
-        </container>`)
+        </container>`);
+};
 
-}
-
-let currentQuestion = store.questions[store.questionNumber]
-let currentAnswer = currentQuestion.answers
+let currentQuestion = store.questions[store.questionNumber];
+let currentAnswer = currentQuestion.answers;
 let questionsTemplate = function () {
   $('main').html(`<container>
             <section class="question-section">
@@ -108,29 +106,41 @@ let questionsTemplate = function () {
                     <h2>${store.questions[store.questionNumber].question}</h2>
                 </div>
                 <div class="question-count">
-                    <span id="count">Question: ${store.questionNumber + 1}/${store.questions.length}</span>
-                    <span id="score">Score: ${store.score}/${store.questions.length}</span>
+                    <span id="count">Question: ${store.questionNumber + 1}/${
+    store.questions.length
+  }</span>
+                    <span id="score">Score: ${store.score}/${
+    store.questions.length
+  }</span>
                 </div>
             </section>
             <section class="question-area">
                 <div class="question-div">
                     <form class="question-list">
-                        <input type="submit" value="${store.questions[store.questionNumber].answers[0]}">
-                        <input type="submit" value="${store.questions[store.questionNumber].answers[1]}">
-                        <input type="submit" value="${store.questions[store.questionNumber].answers[2]}">
-                        <input type="submit" value="${store.questions[store.questionNumber].answers[3]}">
+                        <input type="submit" value="${
+                          store.questions[store.questionNumber].answers[0]
+                        }">
+                        <input type="submit" value="${
+                          store.questions[store.questionNumber].answers[1]
+                        }">
+                        <input type="submit" value="${
+                          store.questions[store.questionNumber].answers[2]
+                        }">
+                        <input type="submit" value="${
+                          store.questions[store.questionNumber].answers[3]
+                        }">
 
 
                     </form>
                 </div>
 
             </section>
-        </container>`)
+        </container>`);
 
   answerQuestion();
-}
+};
 
-let  finishTemplate = function () {
+let finishTemplate = function () {
   $('main').html(`<container>
   <section class="question-section">
 
@@ -146,25 +156,24 @@ let  finishTemplate = function () {
       <button class="restart-button" type="submit">Restart</button>
 
   </section>
-</container>`)
-  restartGame()
-}
+</container>`);
+  restartGame();
+};
 /********** RENDER FUNCTION(S) **********/
 
 // This function conditionally replaces the contents of the <main> tag based on the state of the store
 function render() {
-if (store.quizStarted === false) {
-  $('main').html(startPageTemplate())
-  startGame();
-
-} else if (store.quizStarted === true && store.questionNumber < store.questions.length) {
-$('main').html(questionsTemplate());
-
-
-
-} else {
-  $('main').html(finishTemplate())
-}
+  if (store.quizStarted === false) {
+    $('main').html(startPageTemplate());
+    startGame();
+  } else if (
+    store.quizStarted === true &&
+    store.questionNumber < store.questions.length
+  ) {
+    $('main').html(questionsTemplate());
+  } else {
+    $('main').html(finishTemplate());
+  }
 }
 
 /********** EVENT HANDLER FUNCTIONS **********/
@@ -172,75 +181,62 @@ $('main').html(questionsTemplate());
 // These functions handle events (submit, click, etc)
 
 let startGame = function () {
-
   $('.go-button').click(function () {
     store.quizStarted = true;
 
-    render()
-
-
-  })
-
-}
+    render();
+  });
+};
 
 let answerQuestion = function () {
   store.hasAnswered = false;
   $('input').click(function (event) {
     event.preventDefault();
     if (store.hasAnswered === true) {
-      return
+      return;
     }
     store.hasAnswered = true;
     let guessedAnswer = event.currentTarget.value;
-    console.log(guessedAnswer)
-    console.log(event)
+    console.log(guessedAnswer);
+    console.log(event);
     let correctAnswer = store.questions[store.questionNumber].correctAnswer;
-  if (guessedAnswer === correctAnswer) {
-    console.log('yep')
-    store.score++;
-    store.questionNumber++;
-    console.log(store.score + " " + store.questionNumber);
-    console.log(event)
-$(event.currentTarget).addClass('correct')
+    if (guessedAnswer === correctAnswer) {
+      console.log('yep');
+      store.score++;
+      store.questionNumber++;
+      console.log(store.score + ' ' + store.questionNumber);
+      console.log(event);
+      $(event.currentTarget).addClass('correct');
 
+      setTimeout(function () {
+        render();
+      }, 2000);
+    } else {
+      console.log('nope');
+      $(event.currentTarget).addClass('incorrect');
+      $(`.question-list input[value='${correctAnswer}']`).addClass('correct');
+      store.questionNumber++;
+      console.log(store.score + ' ' + store.questionNumber);
+      setTimeout(function () {
+        render();
+      }, 2000);
+    }
+  });
+};
 
-    setTimeout(function () {
-  render()
-},2000)
-
-
-
-
-  } else {
-    console.log('nope')
-    $(event.currentTarget).addClass('incorrect')
-    $(`.question-list input[value='${correctAnswer}']`).addClass('correct')
-    store.questionNumber++;
-    console.log(store.score + " " + store.questionNumber);
-    setTimeout(function () {
-      render()
-    }, 2000)
-  }
-  })
-
-
-}
-
-let restartGame = function() {
-
+let restartGame = function () {
   $('.restart-button').click(function () {
     store.quizStarted = false;
     store.questionNumber = 0;
     store.score = 0;
-    console.log("test")
-    render()
-
-  })
-}
+    console.log('test');
+    render();
+  });
+};
 function goQuiz() {
   render();
   startGame();
   restartGame();
 }
 
-$(render)
+$(render);
